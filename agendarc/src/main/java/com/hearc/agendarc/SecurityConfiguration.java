8650319951217d504d4d060913e131c174cfaa67
nino.jeannet <nino.jeannet@he-arc.ch>
2020-04-20 17:18:53 +0200

    add security check

--- agendarc/src/main/java/com/hearc/agendarc/SecurityConfiguration.java
+++ agendarc/src/main/java/com/hearc/agendarc/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.hearc.agendarc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
