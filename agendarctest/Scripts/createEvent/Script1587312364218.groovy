ac489d70d881a762cf40ebf5bb361451120dee5a
nino.jeannet <nino.jeannet@he-arc.ch>
2020-04-21 00:09:14 +0200

    fix tests

--- agendarctest/Scripts/createEvent/Script1587312364218.groovy
+++ agendarctest/Scripts/createEvent/Script1587312364218.groovy
@@ -7,7 +7,6 @@ import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
