a755abcec491cccd219029275099638a6513ede4
nino.jeannet <nino.jeannet@he-arc.ch>
2020-04-21 09:37:06 +0200

    fix tests

--- agendarctest/Scripts/createEvent/Script1587312364218.groovy
+++ agendarctest/Scripts/createEvent/Script1587312364218.groovy
@@ -13,7 +13,6 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 
 WebUI.click(findTestObject('page_calendar/link_createEvent'))
 
